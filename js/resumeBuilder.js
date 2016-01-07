aa29b56afd8760c7c4e73267c2b024b1779fdcb3
Moyen <moyen.ruet@gmail.com>
2016-01-07 23:56:14 +0100

    commit on gh-pages

---
+++ js/resumeBuilder.js
@@ -0,0 +1,482 @@
+var bio = {
+	"name" : "Moyen Uddin",
+	"role" : "Web Developer",
+	"contacts" : {
+		"mobile" : "+4917670357043",
+		"email" : "moyen.ruet@gmail.com",
+		"github" : "moyen03",
+		"twitter" : "@moyen03",
+		"linkedin" : "de.linkedin.com/in/moyenuddin",
+		"location" : "Berlin"
+	},
+	"welcomeMessaage" : "I am a friendly, hardworking and clearly dedicated individual who have an ambition to succeed in Web development. I have over 4 years’ experience in Web development covering multiple technologies. I love to learn and I’m happy to work independently or in a team, and apply a positive attitude.",
+	"skills" : ["Programming: PHP, C/C++", "Design: HTML/HTML5, CSS/CSS3, JavaScript, jQuery, Ajax", "CMS: Megento, Wordpress and Typo3", "Database: MySQL, PostgreSQL, DB2, SQL", "Methodology: Design patterns, Algorithms, Data structures, Agile, Software Engineering", "SCM System: SVN, Git"],
+	"bioPic" : "images/biopic.jpg"
+}
+
+var work = {
+	"jobs" : [
+		{
+		"employer" : "Marfione Guitars",
+		"title" : "Freelance Web Developer",
+		"location" : "USA",
+		"dates" : "April 2015 to Present ",
+		"description" : "Responsible for participating in the planning, designing, developing, testing maintaining web based applications. -Investigate and report any operational/technical issues arising. -Scope and document ongoing website functional improvements."
+	    },
+	    {
+		"employer" : "Asytec Dispensers Ltd",
+		"title" : "Web Developer - Online",
+		"location" : "Malta",
+		"dates" : "June 2014 to February 2015 ",
+		"description" : "-Web design and development for several websites. -Regular update and Maintenance."
+	    },
+	    {
+		"employer" : "Rostock University",
+		"title" : "Student Assistant",
+		"location" : "Rostock, Germany",
+		"dates" : "February 2013 to July 2013 ",
+		"description" : "Implementation of a jQuery UI based viewer component for the digital representation for the songs of the songbook in a Typo3 environment. Therefore I had to adapt an existing Typo3 extension to the needs."
+	    },
+	    {
+	    "employer" : "IBM Deutschland Research & Development GmbH",
+		"title" : "Inter Software Engineer",
+		"location" : "Stuttgart, Germany",
+		"dates" : "April 2012 - August 2012 ",
+		"description" : "This is the project for the configuration of IBM Cognos Consumer Insight Tenants. To do this, the whole process contains the 7 different and consequent steps. And each step has the dependency with others."
+	    },
+	    {
+		"employer" : "Divisional Passport & Visa Office",
+		"title" : "Technical Executive",
+		"location" : "Rajshahi, Bangladesh",
+		"dates" : "April 2010 – October 2010 ",
+		"description" : "Maintain the whole VPN Network, SQL Database, LAN and also troubleshooting all of the Computers, Routers, Switches and Server belongs to the Passport Office."
+	    },
+	    {
+		"employer" : "Mams Automation System",
+		"title" : "Programmer",
+		"location" : "Dhaka, Bangladesh",
+		"dates" : "November 2008 - December 2009 ",
+		"description" : "Implementation of client requirements for the new project and customizations and support for the existing web base projects"
+	    }
+	]
+}
+
+var project = {
+	"projects": [
+		{
+		"title": "WossiDiA Project",
+		"location": "University of Rostock, Rostock, Germany",
+		"dates": "December 2012 – June 2013 ",
+		"description": "WossiDiA is a very big project, which is developing as research project under the Institute of Computer Science at University of Rostock, Germany. It is one of the most assorted collections of highly interrelated paper slips documenting regional folk culture. I did my masters thesis on this project. The part to be investigated in this thesis is querying the spatial data such as point, line and region for WossiDiA information system graph data structures.",
+		"images": ["images/wossidia1.png", "images/wossidia2.png"],
+		"technologytitle": "Technologies",
+		"technology": ["PHP,", "JavaScript,", "HTML/CSS,", "PostgreSQl,","PostGIS."]
+		},
+		{
+		"title": "Digital Archive of the Rostock Songbook",
+		"location": "University of Rostock, Rostock, Germany",
+		"dates": "February 2013 to July 2013 ",
+		"description": "This is the project for the configuration of IBM Cognos Consumer Insight Tenants. To do this, the whole process contains the 7 different and consequent steps. And each step has the dependency with others.",
+		"images": ["images/typo1.png", "images/typo2.png"],
+		"technologytitle": "Technologies",
+		"technology": ["PHP/MySQL,", "JavaScript,", "HTML/CSS,", "JQuery,","Typo3,", "SVN."]
+		},
+		{
+		"title": "Administrative Interface at IBM",
+		"location": "Stuttgart, Germany",
+		"dates": "April 2012 – August 2012 ",
+		"description": "This is the project for the configuration of IBM Cognos Consumer Insight Tenants. To do this, the whole process contains the 7 different and consequent steps. And each step has the dependency with others.",
+		"images": [" "],
+		"technologytitle": "Technologies",
+		"technology": ["PHP,", "HTML/CSS,", "JavaScript,", "IBM DB2,", "Apache 2.2 ."]
+		},
+		{
+		"title": "Have many others online project with different languages and CMS",
+		"location": "Germany",
+		"dates": "January 2014 – Present ",
+		"description": "Online Projects on www.upwork.com",
+		"images": ["images/ionia.png", "images/singh.png", "images/smartown.png", "images/tbg.png", "images/gilmore.png", "images/allstringsnylon.jpg"],
+		"technologytitle": "Technologies",
+		"technology": ["PHP/MySQL,", "JavaScript/jQuery,", "HTML/CSS,", "HTML5/CSS3,", "Bootstrap,", "Magento,","Wordpress."]
+		},
+	]
+}
+
+var education = {
+	"schools": [
+		{
+		"name": "Rostock University",
+		"degree": "Masters",
+		"location": "Germany",
+		"majors": ["Computer Science"],
+		"dates": "october 2010 to June 2013 ",
+		"url": "www.uni-rostock.de"
+		},
+		{
+		"name": "Rajshahi University of Engineering and Technology",
+		"degree": "Bacelors",
+		"location": "Bangladesh",
+		"majors": ["Computer Science & Engineering"],
+		"dates": "March 2004 to March 2008 ",
+		"url": "www.ruet.ac.bd"
+		}
+    ],
+    "onlineCourses": [
+		{
+		"title": "German Language A1",
+		"school": "Goethe-Institut Bangladesch",
+		"dates": " June 2010",
+		"url": "https://www.goethe.de/ins/bd/en/spr/kur/gia.html"
+		},
+		{
+		"title": "Front-end Web Developer",
+		"school": "Udacity",
+		"dates": " December 2015",
+		"url": "www.udacity.com"
+		},
+		{
+		"title": "Duolingo German Language Course",
+		"school": "Duolingo",
+		"dates": " December 2015",
+		"url": "https://www.duolingo.com/moyen03"
+		},
+	]
+}
+
+var language = {
+	"languages": [
+		{
+		"name": "English",
+		"fluency": "Fluent"
+		},
+		{
+		"name": "German",
+		"fluency": "Intermediate"
+		},
+		{
+		"name": "Bengali",
+		"fluency": "Native"
+		}
+	]
+}
+
+var additional = {
+	"additionals": [
+		{
+		"additionalskills": ["-Participated in ACM ICPC Regional Contest held December 2007 in Dhaka.", "-Participated in Inter Departmental Programming Contests.", "-Former Vice President, Naogaon District Association, RUET, BANGLADESH.", "-Participated in Google-Workshop (“Creative thinking for Innovation”) at University of Rostock, Germany."]
+		}
+	]
+}
+
+var reference = {
+	"references": [
+		//{
+		//"ref1": ["Dr. Ing. Abbas Malekpour", "Distributed High Performance Computing", "Institute of Computer Science, University of Rostock, Germany.", "Tele: +49-381-498-7544", "Email: abbas.malekpour@gmail.com"]
+		//},
+		{
+		"ref1": ["Dr. Ing. Abbas Malekpour", "Distributed High Performance Computing", "Institute of Computer Science, University of Rostock, Germany.", "Tele: +49-381-498-7544", "Email: abbas.malekpour@gmail.com", "-------------------------------------------------------------------------", "Prof. Dr. Md. Mortuza Ali", "Dean", "Faculty of Engineering & Technology, Eastern University, Bangladesh.", "Phone: +88 02 9677523 Ext. 301", "Mobile: +88 01717-137863", "Email: mmali@easternuni.edu.bd"]
+		}
+	]
+}
+
+
+var formattedHeaderRole = HTMLheaderRole.replace("%data%", bio.role);
+$("#header").prepend(formattedHeaderRole);
+
+var formattedHeaderName = HTMLheaderName.replace("%data%", bio.name);
+$("#header").prepend(formattedHeaderName);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#topContacts").append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#topContacts").append(formattedEmail);
+
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#topContacts").append(formattedGithub);
+
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#topContacts").append(formattedTwitter);
+
+var formattedBlog = HTMLblog.replace("%data%", bio.contacts.linkedin);
+$("#topContacts").append(formattedBlog);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#topContacts").append(formattedLocation);
+
+var formattedBioPic = HTMLbioPic.replace("%data%", bio.bioPic);
+$("#header").append(formattedBioPic);
+
+var formattedWelcomeMsg = HTMLWelcomeMsg.replace("%data%", bio.welcomeMessaage);
+$("#header").append(formattedWelcomeMsg);
+
+if (bio.skills.length > 0) {
+	var i, len;
+	len = bio.skills.length;
+	$("#header").append(HTMLskillsStart);
+		for (i = 0; i < len; i++) {
+		var formattedSkill = HTMLskills.replace("%data%", bio.skills[i]);
+		$("#skills:last").append(formattedSkill);
+		}
+}
+
+function displayWork(){
+
+for(job in work.jobs){
+
+	$("#workExperience").append(HTMLworkStart);
+
+    var formattedEmployer = HTMLworkEmployer.replace("%data%", work.jobs[job].employer);
+    var formattedTitle = HTMLworkTitle.replace("%data%", work.jobs[job].title);
+
+    var formattedEmployerTitle = formattedEmployer+ " " + formattedTitle;
+    $(".work-entry:last").append(formattedEmployerTitle);
+
+    var formattedDates = HTMLworkDates.replace("%data%", work.jobs[job].dates);
+    $(".work-entry:last").append(formattedDates);
+
+    var formattedLocation = HTMLworkLocation.replace("%data%", work.jobs[job].location);
+    $(".work-entry:last").append(formattedLocation);
+
+    var formattedDescription = HTMLworkDescription.replace("%data%", work.jobs[job].description);
+    $(".work-entry:last").append(formattedDescription);
+    }
+}
+
+displayWork();
+
+project.display = function(){
+	for(eachproject in project.projects){
+
+		$("#projects").append(HTMLprojectStart);
+
+		var formattedProjectTitle = HTMLprojectTitle.replace("%data%", project.projects[eachproject].title);
+		$(".project-entry:last").append(formattedProjectTitle);
+
+		var formattedProjectDates = HTMLprojectDates.replace("%data%", project.projects[eachproject].dates);
+		$(".project-entry:last").append(formattedProjectDates);
+
+
+		var formattedProjectDescription = HTMLprojectDescription.replace("%data%", project.projects[eachproject].description);
+		$(".project-entry:last").append(formattedProjectDescription);
+
+		if(project.projects[eachproject].technology.length > 0 ){
+
+			var formattedProjectTechnologyTitle = HTMLprojectTechnologyTitle.replace("%data%", project.projects[eachproject].technologytitle);
+
+			var i, len;
+			var formattedProjectTechnology = " ";
+			len = project.projects[eachproject].technology.length;
+			for (i=0; i<len;i++){
+				formattedProjectTechnology +=  project.projects[eachproject].technology[i]+"  ";
+			}
+			var formattedProjectTechnology = HTMLprojectTechnology.replace("%data%", formattedProjectTechnology);
+
+			var formattedProjectTechnologyTitleAndTechnology = formattedProjectTechnologyTitle+" "+" "+formattedProjectTechnology;
+			$(".project-entry:last").append(formattedProjectTechnologyTitleAndTechnology);
+		}
+
+		if(project.projects[eachproject].images.length > 0 ){
+			var i, len;
+			len = project.projects[eachproject].images.length;
+			for (i=0; i<len;i++){
+				var formattedProjectImage = HTMLprojectImage.replace("%data%", project.projects[eachproject].images[i]);
+				$(".project-entry:last").append(formattedProjectImage);
+			}
+		}
+	}
+}
+project.display();
+
+education.display = function(){
+
+	for(eachschool in education.schools){
+
+	  $("#education").append(HTMLschoolStart);
+
+	    var formattedSchoolName = HTMLschoolName.replace("%data%", education.schools[eachschool].name);
+		var formattedSchoolDegree = HTMLschoolDegree.replace("%data%", education.schools[eachschool].degree);
+
+		var formattedSchoolNameAndDegree = formattedSchoolName+" "+formattedSchoolDegree;
+
+		$(".education-entry:last").append(formattedSchoolNameAndDegree);
+
+		var formattedSchoolDates = HTMLschoolDates.replace("%data%", education.schools[eachschool].dates);
+		$(".education-entry:last").append(formattedSchoolDates);
+
+		var formattedSchoolLocation = HTMLschoolLocation.replace("%data%", education.schools[eachschool].location);
+		$(".education-entry:last").append(formattedSchoolLocation);
+
+
+		if(education.schools[eachschool].majors.length > 0 ){
+			var i, len;
+			var formattedSchoolMajor = " ";
+			len = education.schools[eachschool].majors.length;
+			for (i=0; i<len;i++){
+				formattedSchoolMajor +=  education.schools[eachschool].majors[i]+"  ";
+			}
+			var formattedSchoolMajor = HTMLschoolMajor.replace("%data%", formattedSchoolMajor);
+			$(".education-entry:last").append(formattedSchoolMajor);
+		}
+
+		var formattedSchoolUrl = HTMLschoolURL.replace("%data%", education.schools[eachschool].url);
+		$(".education-entry:last").append(formattedSchoolUrl);
+	}
+
+	if (education.onlineCourses.length > 0){
+		$("#education").append(HTMLonlineClasses);
+
+		for(eachonlinecourse in education.onlineCourses){
+
+		    var formattedOnlineTitle = HTMLonlineTitle.replace("%data%", education.onlineCourses[eachonlinecourse].title);
+			var formattedOnlineSchool = HTMLonlineSchool.replace("%data%", education.onlineCourses[eachonlinecourse].school);
+
+			var formattedOnlineTitleAndSchool = formattedOnlineTitle+" "+formattedOnlineSchool;
+
+			$(".online-entry:last").append(formattedOnlineTitleAndSchool);
+
+		    var formattedOnlineDates = HTMLonlineDates.replace("%data%", education.onlineCourses[eachonlinecourse].dates);
+			$(".online-entry:last").append(formattedOnlineDates);
+
+			var formattedOnlineURL = HTMLonlineURL.replace("%data%", education.onlineCourses[eachonlinecourse].url);
+			$(".online-entry:last").append(formattedOnlineURL);
+		}
+	}
+}
+
+education.display();
+
+
+language.display = function(){
+
+	if (language.languages.length > 0){
+		$("#language").append(HTMLlanguageStart);
+
+		for(eachlanguage in language.languages){
+
+		    var formattedLanguageName = HTMLlanguageName.replace("%data%", language.languages[eachlanguage].name);
+			var formattedLanguageFluency = HTMLlanguageFluency.replace("%data%", language.languages[eachlanguage].fluency);
+
+			var formattedLanguageNameAndFluency = formattedLanguageName+" "+formattedLanguageFluency+"<br>";
+
+			$(".language-entry:last").append(formattedLanguageNameAndFluency);
+		}
+	}
+
+}
+
+language.display();
+
+additional.display = function(){
+
+	if (additional.additionals.length > 0){
+		$("#additional").append(HTMLadditionalStart);
+
+		for(eachadditional in additional.additionals){
+			var len = additional.additionals[eachadditional].additionalskills.length;
+			for (var i=0; i<len; i++){
+				var formattedAdditionalName = HTMLadditionalName.replace("%data%", additional.additionals[eachadditional].additionalskills[i]);
+				$(".additional-entry:last").append(formattedAdditionalName);
+			}
+		}
+	}
+}
+
+additional.display();
+
+reference.display = function(){
+	if (reference.references.length > 0){
+		$("#reference").append(HTMLreferenceStart);
+
+		for(eachreference in reference.references){
+			var lenref1 = reference.references[eachreference].ref1.length;
+			if(lenref1 > 0){
+				for (var i=0; i<lenref1; i++){
+					var formattedReferenceName = HTMLreferencelName.replace("%data%", reference.references[eachreference].ref1[i]);
+					$(".reference-entry:last").append(formattedReferenceName);
+				}
+			}
+		}
+		/*for(eachreference in reference.references){
+			var lenref2 = reference.references[eachreference].ref2.length;
+			if(lenref2 > 0){
+				for (var i=0; i<lenref2; i++){
+					var formattedReferenceName = HTMLreferencelName.replace("%data%", reference.references[eachreference].ref2[i]);
+					$(".reference-entry:last").append(formattedReferenceName);
+				}
+			}
+		} */
+	}
+}
+
+reference.display();
+
+$("#mapDiv").append(googleMap);
+
+var formattedMobile = HTMLmobile.replace("%data%", bio.contacts.mobile);
+$("#footerContacts").append(formattedMobile);
+
+var formattedEmail = HTMLemail.replace("%data%", bio.contacts.email);
+$("#footerContacts").append(formattedEmail);
+
+var formattedGithub = HTMLgithub.replace("%data%", bio.contacts.github);
+$("#footerContacts").append(formattedGithub);
+
+var formattedTwitter = HTMLtwitter.replace("%data%", bio.contacts.twitter);
+$("#footerContacts").append(formattedTwitter);
+
+var formattedBlog = HTMLblog.replace("%data%", bio.contacts.linkedin);
+$("#footerContacts").append(formattedBlog);
+
+var formattedLocation = HTMLlocation.replace("%data%", bio.contacts.location);
+$("#footerContacts").append(formattedLocation);
+
+
+$(document).click(function(loc){
+	var x = loc.pageX;
+	var y = loc.pageY;
+
+	logClicks(x,y);
+
+});
+
+function locationizer(work_obj){
+        var locationArray = [];
+        for(jobloc in work_obj.jobs) {
+        	var newlocation = work_obj.jobs[jobloc].location;
+         	locationArray.push(newlocation);
+        }
+    return locationArray;
+}
+
+locationizer(work);
+
+//console.log(locationizer(work));
+
+//$("#main").append(internationalizeButton);
+
+/*
+inName("sebastian Thurn");
+
+function inName(fullName){
+	var newArrayName = fullName.split(" ");
+	//console.log(newArrayName);
+	var fName = newArrayName[0];
+	//console.log(fName);
+	var lName = newArrayName[1].toUpperCase();
+	//console.log(lName);
+    return fName +" "+ lName;
+    //console.log(intName);
+} */
+
+inName("sebastian Thurn");
+
+function inName(name){
+	name = name.split(" ");
+	//console.log(name);
+	name[1] = name[1].toUpperCase();
+	name[0] = name[0].slice(0, 1).toUpperCase() + name[0].slice(1).toLowerCase();
+	//console.log(name[0]);
+	return name[0]+" "+name[1];
+}
+
